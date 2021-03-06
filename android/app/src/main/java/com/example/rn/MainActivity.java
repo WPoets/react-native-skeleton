3803d4664b822e6df6eb592f31bc7494bf5ac866
Sooraj <soorajnraju@gmail.com>
2021-03-06 08:56:04 +0530

    Disabled Screen Shot

--- android/app/src/main/java/com/example/rn/MainActivity.java
+++ android/app/src/main/java/com/example/rn/MainActivity.java
@@ -15,6 +15,13 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "RNSkelton";
   }
+
+  // Add this method.
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE);
+  }
   
   // Override onStart, onNewIntent:
   @Override
